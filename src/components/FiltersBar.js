d64a12b522391ba55596b22f9dc9e599db75c78e
FloareDeMai <matei.g.florentina@gmail.com>
2021-11-10 22:04:58 +0200

    added style when a filter is active

--- src/components/FiltersBar.js
+++ src/components/FiltersBar.js
@@ -53,86 +53,184 @@ function FiltersBar() {
 
   return (
     <div className="flex mt-4 items-center">
-      <button
-        className="flex mr-6 hover-filters "
-        type="submit"
-        onClick={getAllHotels}
-      >
-        <img
-          src="../../images/pet-house.png"
-          alt="pethouse"
-          objectfit="contain"
-          objectposition="left"
-          className="h-6 mr-1"
-        />
-        <h1 className="text-2xl text-right" style={{ fontFamily: "Raleway" }}>
-          Hotels
-        </h1>
-      </button>
-      <div className="flex mr-6 hover-filters">
-        <img
-          src="../../images/dog.png"
-          alt="pethouse"
-          objectfit="contain"
-          objectposition="left"
-          className="h-6"
-        />
+      {filterParams.length === 0 ? (
         <button
-          className="text-2xl text-right"
-          style={{ fontFamily: "Raleway" }}
-          onClick={() => getAllPetHotelsByFilterType("DOGS")}
+          className="flex mr-6 active-button"
+          type="submit"
+          onClick={getAllHotels}
         >
-          For dogs
+          <img
+            src="../../images/pet-house.png"
+            alt="pethouse"
+            objectfit="contain"
+            objectposition="left"
+            className="h-6 mr-1"
+          />
+          <h1 className="text-2xl text-right" style={{ fontFamily: "Raleway" }}>
+            Hotels
+          </h1>
         </button>
-      </div>
-      <div className="flex mr-6 hover-filters">
-        <img
-          src="../../images/cat.png"
-          alt="pethouse"
-          objectfit="contain"
-          objectposition="left"
-          className="h-6"
-        />
+      ) : (
         <button
-          className=" text-2xl text-right"
-          style={{ fontFamily: "Raleway" }}
-          onClick={() => getAllPetHotelsByFilterType("CATS")}
+          className="flex mr-6 hover-filters"
+          type="submit"
+          onClick={getAllHotels}
         >
-          For cats
+          <img
+            src="../../images/pet-house.png"
+            alt="pethouse"
+            objectfit="contain"
+            objectposition="left"
+            className="h-6 mr-1"
+          />
+          <h1 className="text-2xl text-right" style={{ fontFamily: "Raleway" }}>
+            Hotels
+          </h1>
         </button>
-      </div>
-      <div className="flex mr-6 hover-filters">
-        <img
-          src="../../images/fences.png"
-          alt="pethouse"
-          objectfit="contain"
-          objectposition="left"
-          className="h-6 mr-1"
-        />
-        <button
-          className="text-2xl text-right"
-          style={{ fontFamily: "Raleway" }}
-          onClick={() => getAllPetHotelsByFilterType("GARDEN")}
-        >
-          With a garden
-        </button>
-      </div>
-      <div className="flex mr-6 hover-filters">
-        <img
-          src="../../images/veterinarian.png"
-          alt="pethouse"
-          objectfit="contain"
-          objectposition="left"
-          className="h-6 mr-1"
-        />
-        <button
-          className="text-2xl text-right"
-          style={{ fontFamily: "Raleway" }}
-          onClick={() => getAllPetHotelsByFilterType("VET")}
-        >
-          Has veterinary
-        </button>
-      </div>
+      )}
+
+      {filterParams.indexOf("DOGS") > -1 ? (
+        <div className="flex mr-6 active-button">
+          <img
+            src="../../images/dog.png"
+            alt="pethouse"
+            objectfit="contain"
+            objectposition="left"
+            className="h-6"
+          />
+          <button
+            className="text-2xl text-right"
+            style={{ fontFamily: "Raleway" }}
+            onClick={() => getAllPetHotelsByFilterType("DOGS")}
+          >
+            For dogs
+          </button>
+        </div>
+      ) : (
+        <div className="flex mr-6 hover-filters">
+          <img
+            src="../../images/dog.png"
+            alt="pethouse"
+            objectfit="contain"
+            objectposition="left"
+            className="h-6"
+          />
+          <button
+            className="text-2xl text-right"
+            style={{ fontFamily: "Raleway" }}
+            onClick={() => getAllPetHotelsByFilterType("DOGS")}
+          >
+            For dogs
+          </button>
+        </div>
+      )}
+
+      {filterParams.indexOf("CATS") > -1 ? (
+        <div className="flex mr-6 active-button">
+          <img
+            src="../../images/cat.png"
+            alt="pethouse"
+            objectfit="contain"
+            objectposition="left"
+            className="h-6"
+          />
+          <button
+            className=" text-2xl text-right"
+            style={{ fontFamily: "Raleway" }}
+            onClick={() => getAllPetHotelsByFilterType("CATS")}
+          >
+            For cats
+          </button>
+        </div>
+      ) : (
+        <div className="flex mr-6 hover-filters">
+          <img
+            src="../../images/cat.png"
+            alt="pethouse"
+            objectfit="contain"
+            objectposition="left"
+            className="h-6"
+          />
+          <button
+            className=" text-2xl text-right"
+            style={{ fontFamily: "Raleway" }}
+            onClick={() => getAllPetHotelsByFilterType("CATS")}
+          >
+            For cats
+          </button>
+        </div>
+      )}
+
+      {filterParams.indexOf("GARDEN") > -1 ? (
+        <div className="flex mr-6 active-button">
+          <img
+            src="../../images/fences.png"
+            alt="pethouse"
+            objectfit="contain"
+            objectposition="left"
+            className="h-6 mr-1"
+          />
+          <button
+            className="text-2xl text-right"
+            style={{ fontFamily: "Raleway" }}
+            onClick={() => getAllPetHotelsByFilterType("GARDEN")}
+          >
+            With a garden
+          </button>
+        </div>
+      ) : (
+        <div className="flex mr-6 hover-filters">
+          <img
+            src="../../images/fences.png"
+            alt="pethouse"
+            objectfit="contain"
+            objectposition="left"
+            className="h-6 mr-1"
+          />
+          <button
+            className="text-2xl text-right"
+            style={{ fontFamily: "Raleway" }}
+            onClick={() => getAllPetHotelsByFilterType("GARDEN")}
+          >
+            With a garden
+          </button>
+        </div>
+      )}
+      {filterParams.indexOf("VET") > -1 ? (
+        <div className="flex mr-6 active-button">
+          <img
+            src="../../images/veterinarian.png"
+            alt="pethouse"
+            objectfit="contain"
+            objectposition="left"
+            className="h-6 mr-1"
+          />
+          <button
+            className="text-2xl text-right"
+            style={{ fontFamily: "Raleway" }}
+            onClick={() => getAllPetHotelsByFilterType("VET")}
+          >
+            Has veterinary
+          </button>
+        </div>
+      ) : (
+        <div className="flex mr-6 hover-filters">
+          <img
+            src="../../images/veterinarian.png"
+            alt="pethouse"
+            objectfit="contain"
+            objectposition="left"
+            className="h-6 mr-1"
+          />
+          <button
+            className="text-2xl text-right"
+            style={{ fontFamily: "Raleway" }}
+            onClick={() => getAllPetHotelsByFilterType("VET")}
+          >
+            Has veterinary
+          </button>
+        </div>
+      )}
     </div>
   );
 }
